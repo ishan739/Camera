f79c3630210e620f5f8ca07b881f6546ae8389f0
ishan739 <gupta.ishan2004@gmail.com>
2025-02-08 22:05:26 +0530

    Use of Coroutines

--- app/src/main/java/com/example/camerax/CameraScreen.kt
+++ app/src/main/java/com/example/camerax/CameraScreen.kt
@@ -1,9 +1,12 @@
 package com.example.camerax
 
 import android.content.ContentValues
+import android.content.Context
 import android.graphics.Color
 import android.net.Uri
 import android.os.Environment
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout
@@ -27,6 +30,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun CameraScreen(){
@@ -46,36 +52,11 @@ fun CameraScreen(){
                         object : ImageCapture.OnImageCapturedCallback() {
                             override fun onCaptureSuccess(image: ImageProxy) {
                                 super.onCaptureSuccess(image)
-                                val contentValues = ContentValues().apply{
-                                    put(MediaStore.Images.Media.DISPLAY_NAME, "IMG_${System.currentTimeMillis()}.jpg")
-                                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-                                    put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    saveImageToGallery(context, image)
 
                                 }
-                                val uri : Uri? = context.contentResolver
-                                    .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
-                                uri?.let {
-                                    context.contentResolver.openOutputStream(it)
-                                        ?.use { outputStream ->
-                                            val buffer = image.planes[0].buffer
-                                            val bytes = ByteArray(buffer.remaining())
-                                            buffer.get(bytes)
-                                            outputStream.write(bytes)
-                                        }
-                                    Toast.makeText(
-                                        context,
-                                        "Image saved to gallery",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                } ?: run {
-                                    Toast.makeText(
-                                        context,
-                                        "Failed to save image",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                                image.close()
                             }
 
                             override fun onError(exception: ImageCaptureException) {
@@ -111,6 +92,38 @@ fun CameraScreen(){
             }
         })
     }
+}
+
+private fun saveImageToGallery(context: Context, image: ImageProxy){
+
+    val buffer = image.planes[0].buffer
+    val bytes = ByteArray(buffer.remaining())
+    buffer.get(bytes)
+
+    val contentValues = ContentValues().apply{
+        put(MediaStore.Images.Media.DISPLAY_NAME, "IMG_${System.currentTimeMillis()}.jpg")
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+
+    }
+    val uri : Uri? = context.contentResolver
+        .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+    uri?.let {
+        context.contentResolver.openOutputStream(it)
+            ?.use { outputStream ->
+                outputStream.write(bytes)
+                outputStream.flush()
+            }
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(context, "Image Saved!", Toast.LENGTH_SHORT).show()
+        }
+    } ?: run {
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(context, "Failed To Save!", Toast.LENGTH_SHORT).show()
+        }
+    }
+    image.close()
 }
 
 
