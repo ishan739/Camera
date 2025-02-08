6dd3498c6aac74a146b696f9b9d174d06554990c
ishan739 <gupta.ishan2004@gmail.com>
2025-02-08 22:58:02 +0530

    Switch Camera Btn

--- app/src/main/java/com/example/camerax/CameraScreen.kt
+++ app/src/main/java/com/example/camerax/CameraScreen.kt
@@ -11,25 +11,30 @@ import android.provider.MediaStore
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -45,58 +50,94 @@ fun CameraScreen(){
     val cameraController = remember { LifecycleCameraController(context) }
     val mainExecuter = ContextCompat.getMainExecutor(context)
 
+    var isFrontCamera by remember { mutableStateOf(false) }
+
     Scaffold (
         modifier = Modifier.fillMaxSize(),
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    cameraController.takePicture(
-                        mainExecuter,
-                        object : ImageCapture.OnImageCapturedCallback() {
-                            override fun onCaptureSuccess(image: ImageProxy) {
-                                super.onCaptureSuccess(image)
 
-                                CoroutineScope(Dispatchers.IO).launch {
-                                    saveImageToGallery(context, image)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ){
 
-                                }
-                            }
-
-                            override fun onError(exception: ImageCaptureException) {
-                                super.onError(exception)
-                                Toast.makeText(
-                                    context,
-                                    "Error Occurred in CameraX",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
+                FloatingActionButton(
+                    onClick = {
+                        isFrontCamera = !isFrontCamera
+                        var cameraSelector = if (isFrontCamera){
+                            CameraSelector.DEFAULT_FRONT_CAMERA
+                        } else {
+                            CameraSelector.DEFAULT_BACK_CAMERA
                         }
+
+                        cameraController.cameraSelector = cameraSelector
+                        cameraController.bindToLifecycle(lifecycleOwner)
+
+                    }
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_switch),
+                        contentDescription = "Switch Camera"
                     )
                 }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_camera),
-                    contentDescription = "Capture Image"
-                )
+
+                FloatingActionButton(
+                    onClick = {
+                        cameraController.takePicture(
+                            mainExecuter,
+                            object : ImageCapture.OnImageCapturedCallback() {
+                                override fun onCaptureSuccess(image: ImageProxy) {
+                                    super.onCaptureSuccess(image)
+
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        saveImageToGallery(context, image)
+
+                                    }
+                                }
+
+                                override fun onError(exception: ImageCaptureException) {
+                                    super.onError(exception)
+                                    Toast.makeText(
+                                        context,
+                                        "Error Occurred in CameraX",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_camera),
+                        contentDescription = "Capture Image"
+                    )
+                }
             }
+
+
 
         }
 
         ){paddingValues ->
-        AndroidView(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            factory = {context ->
-            PreviewView(context).apply {
-                layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-                setBackgroundColor(Color.BLACK)
-                scaleType = PreviewView.ScaleType.FIT_START
-            }.also {previewView ->
-                previewView.controller = cameraController
-                cameraController.bindToLifecycle(lifecycleOwner)
-            }
-        })
+        Column(
+            modifier = Modifier.fillMaxSize().padding(paddingValues)
+        )
+        {
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                factory = { context ->
+                    PreviewView(context).apply {
+                        layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                        setBackgroundColor(Color.BLACK)
+                        scaleType = PreviewView.ScaleType.FIT_CENTER
+                    }.also { previewView ->
+                        previewView.controller = cameraController
+                        cameraController.bindToLifecycle(lifecycleOwner)
+                    }
+                })
+        }
     }
 }
 
